8f9fe58b6c2d2b9f04677627e8aa86c09b1234d9
Kariuki79 <kariukimwangi79@gmail.com>
2023-11-22 06:34:48 -0800

    update 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
-import sys
 from __future__ import print_function
+import sys
 
 def safe_function(fct, *args):
     try:
         a = fct(*args)
-        return a
     except Exception as error:
         print("Exception: {}".format(error), file=sys.stderr)
         return None
