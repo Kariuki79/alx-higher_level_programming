f8a41ea8d1d62a7742fcb5d2425d3001149638ca
Kariuki79 <kariukimwangi79@gmail.com>
2023-11-29 12:50:01 -0800

    update

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,8 +5,7 @@
 
 class Rectangle:
     """Represent a class rectangle"""
-
-     number_of_instances = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initialize a new instance rectangle """
