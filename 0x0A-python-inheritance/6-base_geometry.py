0ef8c803014736984488f6648e1c9007a9f2ec33
Kariuki79 <kariukimwangi79@gmail.com>
2023-12-05 11:12:11 -0800

    New file 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Class called BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """A class with public attributes"""
+    def area(self):
+        """Exception"""
+        raise Exception("area() is not implemented")
