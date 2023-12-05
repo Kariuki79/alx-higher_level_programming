cee19e9aebed7688fd15d55a859e0d46b63d1a23
Kariuki79 <kariukimwangi79@gmail.com>
2023-12-05 11:02:32 -0800

    New file 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Function inherits_from(obj, a_class)
+"""
+
+
+def inherits_from(obj, a_class):
+    """returns true if the object is an instance of a class"""
+    return(issubclass(type(obj), a_class) and type(obj) != a_class)
