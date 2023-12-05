d102cdb0be54cdf3f87f60601d477930520be4d2
Kariuki79 <kariukimwangi79@gmail.com>
2023-12-05 10:44:24 -0800

    New file

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A Class that inherits from the list """
+
+
+class MyList(list):
+    """ A class that inherits from the list """
+
+    def print_sorted(self):
+        print(sorted(self))
