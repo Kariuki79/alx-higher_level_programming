a55221efbf691e7416ef71612b429d5576e004c6
Kariuki79 <kariukimwangi79@gmail.com>
2023-12-05 11:57:47 -0800

    Python - Input/Output

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Function append_after"""
+
+
+def append_after(file_name="", p_search_tring="", p_new_string=""):
+    """A function that inserts a line of text to a file, after each line containing a specific string"""
+    with open(file_name, "r") as f:
+        text = f.readlines()
+
+    with open(file_name, "w") as fo:
+        s = ""
+        for line in text:
+            s += line
+            if p_search_tring in line:
+                s += p_new_string
+        fo.write(s)
