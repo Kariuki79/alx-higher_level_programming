d351a9ecf7dee60bd5f1bd55a2195b1db8b84989
Kariuki Mwangi <139071098+Kariuki79@users.noreply.github.com>
2024-04-25 06:01:17 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-// Import the built-in Node.js 'fs' module.
+const file = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  // Use fs.readFile() to read the contents of a file specified as a command-line argument
-  // 'utf8' specifies the encoding of the file being read
+const filePath = process.argv[2];
 
-  if (error) {
-    // If an error occurs during the file read operation, the 'error' parameter will contain an error object.
-    console.error('Error reading the file:', error);
-
+if (!filePath) {
+  console.error('Error: Please provide a file path');
+  process.exit(1);
+}
+file.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('error reading file', err);
   } else {
-    // If the file is read successfully, the 'content' parameter will contain the contents of the file as a string.
-    console.log(content);
+    console.log(data);
   }
 });
