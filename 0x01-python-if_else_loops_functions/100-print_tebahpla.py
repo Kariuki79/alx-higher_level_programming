c908fd33dff62f085580666f2ba65040faa968aa
Kariuki79 <kariukimwangi75@gmail.com>
2025-08-25 02:00:59 -0700

    Add solution for 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-print("".join(["{}".format(chr(c - 32) if c % 2 else chr(c))
-    for c in range(122, 96, -1)]), end="")
+print("".join(
+    ["{}".format(chr(c - 32) if c % 2 else chr(c))
+     for c in range(122, 96, -1)]
+), end="")
