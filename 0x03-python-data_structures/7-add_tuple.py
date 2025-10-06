72e614856e9786c942e84cd605b5e274e033b07f
Kariuki79 <kariukimwangi75@gmail.com>
2025-10-06 07:27:16 -0700

    a function that adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    """Adds two tuples and returns a new tuples with 2 integers"""
+    """Adds two tuple and returns a new tuples with 2 integers"""
     # Extend both tuples to have at least 2 elements
     a = tuple_a + (0, 0)
     b = tuple_b + (0, 0)
